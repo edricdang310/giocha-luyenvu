324c98e328d061a6961b2694c512d893536a757e
edricdang310 <dhthinh.cntt@gmail.com>
2026-07-09 11:10:41 +0700

    feat: add static sitemap.xml and disable index sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://giochaluyenvu.shop",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   robotsTxtOptions: {
     policies: [
       {
